a34eca060dbd13d1e5872f2d14c388fcee57c868
Unnati17 <67010484+Unnati17@users.noreply.github.com>
2022-01-19 20:16:27 +0530

    Add files via upload

---
+++ students.js
@@ -0,0 +1,170 @@
+[
+    {
+        name:"John",
+        subjects:["phy","chem","maths"],
+        rollno:30,
+        class:12,
+        marks:[
+            {
+                theory:67,
+                pracitcal:10
+
+            },
+            {
+                theory: 57,
+                pracitcal: 20
+            }, 
+            {
+                theory: 70,
+                pracitcal: 15
+            }
+        ]
+    },
+    {
+        name: "Sam",
+        subjects: ["phy", "chem", "bio"],
+        rollno: 42,
+        class:12,
+        marks: [
+            {
+                theory: 70,
+                pracitcal: 20
+
+            },
+            {
+                theory: 65,
+                pracitcal: 10
+            },
+            {
+                theory: 56,
+                pracitcal: 15
+            }
+        ]
+    },
+    {
+        name: "Sham",
+        subjects: ["eco", "stats", "maths"],
+        rollno: 35,
+        class:12,
+        marks: [
+            {
+                theory: 70,
+                pracitcal: 15
+
+            },
+            {
+                theory: 50,
+                pracitcal: 10
+            },
+            {
+                theory: 44,
+                pracitcal: 5
+            }
+        ]
+    },
+    {
+        name: "Nick",
+        subjects: ["phy", "chem", "maths","bio"],
+        rollno: 40,
+        class:12,
+        marks: [
+            {
+                theory: 66,
+                pracitcal: 10
+
+            },
+            {
+                theory: 55,
+                pracitcal: 15
+            },
+            {
+                theory: 34,
+                pracitcal: 20
+            }
+        ]
+    },
+    {
+        name: "John",
+        subjects: ["phy", "chem", "maths"],
+        rollno: 30,
+        class: 11,
+        marks: [
+            {
+                theory: 54,
+                pracitcal: 10
+
+            },
+            {
+                theory: 60,
+                pracitcal: 20
+            },
+            {
+                theory: 67,
+                pracitcal: 15
+            }
+        ]
+    },
+    {
+        name: "Sam",
+        subjects: ["phy", "chem", "bio"],
+        rollno: 42,
+        class: 11,
+        marks: [
+            {
+                theory: 69,
+                pracitcal: 20
+
+            },
+            {
+                theory: 70,
+                pracitcal: 30
+            },
+            {
+                theory: 67,
+                pracitcal: 15
+            }
+        ]
+    },
+    {
+        name: "Sham",
+        subjects: ["eco", "stats", "maths"],
+        rollno: 35,
+        class: 11,
+        marks: [
+            {
+                theory: 56,
+                pracitcal: 10
+
+            },
+            {
+                theory: 56,
+                pracitcal: 15
+            },
+            {
+                theory: 54,
+                pracitcal: 10
+            }
+        ]
+    },
+    {
+        name: "Nick",
+        subjects: ["phy", "chem", "maths", "bio"],
+        rollno: 40,
+        class: 12,
+        marks: [
+            {
+                theory: 69,
+                pracitcal: 15
+
+            },
+            {
+                theory: 60,
+                pracitcal: 30
+            },
+            {
+                theory: 40,
+                pracitcal: 15
+            }
+        ]
+    }
+]
